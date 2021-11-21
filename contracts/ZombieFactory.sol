67c51b1220c143cd1a6c557e3b090e5d88b7a528
truthgarnet <dmstj105@gmail.com>
2021-11-21 23:21:58 +0900

    Chapture 3

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -1,16 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.10;
 
-contract ZombieFactory {
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "..\node_modules\zeppelin-solidity\contracts\ownership\Ownable.sol";
+
+contract ZombieFactory is Ownable{
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+    uint cooldownTime = 1 days;
 
     event NewZombie(uint zombieId, string name, uint dna);
 
     struct Zombie{
         string name;
         uint256 dna;
+        uint32 level;
+        uint32 readyTime;
     }
 
     Zombie[] public zombies;
@@ -18,8 +24,8 @@ contract ZombieFactory {
     mapping (uint => address) public zombieToOwner;
     mapping (address => uint) ownerZombieCount;
 
-    function _createZombie(string storage _name, uint256 _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+    function _createZombie(string memory _name, uint256 _dna, uint32 level, uint32 cooldownTime) internal {
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
         NewZombie(id, _name, _dna);
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
